699fea0c4093708cda991550bea84779244fa735
mazhar zeb <72321254+mazhar-bit@users.noreply.github.com>
2024-06-24 11:31:42 +0000

    array

--- project/Arrays.js
+++ project/Arrays.js
@@ -12,5 +12,25 @@ const arr2 = new Array(1,2,3,4);
 
 const arr3=[1,2,3,4,5]
 
-arr3.push(7)
-console.log(arr3);
+arr3.push(7)            
+arr3.pop()                  // it delete the value from the end of the array 
+
+arr3.unshift(9)             // it add the value in start of the arrry
+
+arr3.shift()                // the delete the value from the start in // 
+
+// console.log(arr3.includes(4));      // its help to check the value in array and reture boolean value
+
+// console.log(arr3.indexOf(0));        // find the index value  which not exixted in the array its return the -1 value other wise give you correct value
+
+const newarr = arr3.join()           // join() help to bend the array and convert in to string from "check the typeof array"
+
+// console.log(arr3);
+// console.log(newarr);
+// console.log(typeof(newarr))
+
+
+// ============== slice ==== splice ===================///
+
+
+

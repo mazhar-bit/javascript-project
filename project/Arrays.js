c093e3667a8a835108b3115f94148dbb4c4dbbce
mazhar zeb <72321254+mazhar-bit@users.noreply.github.com>
2024-06-24 09:16:28 +0000

    Arrays

---
+++ project/Arrays.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,4]
+
+// console.log(arr[1]);
+
+const arr2 = new Array(1,2,3,4);
+
+
+
+
+// Array method  +____++ THIS IS MORE IMPORTANT+++-----++++
+
+
+const arr3=[1,2,3,4,5]
+
+arr3.push(7)
+console.log(arr3);
---
+++ project/string.js
@@ -0,0 +1,3 @@
+let name = 'jhsonn'
+console.log(name.length);
+

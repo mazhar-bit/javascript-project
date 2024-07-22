95dadab479b37d03bc6177b227606027deb85ab5
mazhar zeb <72321254+mazhar-bit@users.noreply.github.com>
2024-07-22 12:05:07 +0000

    objects

---
+++ project/Object.js
@@ -0,0 +1,25 @@
+// object literals
+
+const stduser = {
+    name:'std1',  // Key : vaule
+    age:18,
+    location: 'alsak',
+    email:'yahii@.com',
+    Logindata:['mon','tue'],
+    welcomefuction:  () =>{
+        console.log("yesss..")
+    }
+}
+// two way to access the object vaule 
+ console.log(stduser.name);
+ console.log(stduser['age']);
+
+ // function log
+console.log(stduser.welcomefuction());
+
+stduser.welcomeftn2 = () =>{
+
+    console.log(`welcome student ${stduser.name}`)
+}
+ 
+console.log(stduser.welcomeftn2());
